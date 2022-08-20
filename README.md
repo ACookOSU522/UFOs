d5e37e028d27720f8503a86276dc2fc33e1742f0
Alison Cook <Crlygrly0182@msn.com>
2022-08-20 18:59:25 -0400

    mod work up thru 11.5.2

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # UFOs
+## Overview
+### Purpose
+
+## Resources
+
+## Results/Analysis
+Describe to Dana how someone might use the new webpage by walking her through the process of using the search criteria. Use images of your webpage during the filtering process to support your explanation.
+There is a description of how to perform a search, with images.
+
+## Summary
+The summary addresses one drawback of this webpage (2 pt)
+The summary addresses two additional recommendations for further development
--- static/js/app.js
+++ static/js/app.js
@@ -48,10 +48,57 @@ function doubleAddition(c, d) {
     return total;
   };
 
+  // converted to arrow
+  doubleAddition = (c, d)=> addition(c, d) * 2;
+
+  //skill drill Use the newly refactored doubleAddition function to find the total of 33 and 25.
+  doubleAddition = (35, 25)=> addition(35, 25) * 2;
+
   let friends = ["Sarah", "Greg", "Cindy", "Jeff"];
   function listLoop(userList) {
     for (var i = 0; i < userList.length; i++) {
       console.log(userList[i]);
     }
  };
+
+ // to clear browser
  clear()
+
+//forloops
+ let friends = ["Sarah", "Greg", "Cindy", "Jeff"];
+ function listLoop(userList) {
+  for (var i = 0; i < userList.length; i++) {
+    console.log(userList[i]);
+  }
+};
+
+var vegetables = ["Carrots", "Peas", "Lettuce", "Tomatoes"];
+
+for (var i = 0; i < vegetables.length; i++) {
+    console.log("I love " + vegetables[i]);
+};
+
+for (var i = 0; i < 5; i++) {
+  console.log("I am " + i);
+};
+
+//BUILD TABLE step 1
+function buildTable(data) {
+  // First, clear out any existing data
+  tbody.html("");
+
+  // Next, loop through each object in the data
+  // and append a row and cells for each value in the row
+  data.forEach((dataRow) => {
+    // Append a row to the table body
+    let row = tbody.append("tr");
+
+    // Loop through each field in the dataRow and add
+    // each value as a table cell (td)
+    Object.values(dataRow).forEach((val) => {
+      let cell = row.append("td");
+      cell.text(val);
+      }
+    );
+  });
+};
