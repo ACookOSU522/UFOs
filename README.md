fbf103e0443e3bbb8f95759aebd79bd89a3bf445
Alison Cook <Crlygrly0182@msn.com>
2022-08-21 17:22:27 -0400

    challenge 11 UFO work

--- README.md
+++ README.md
@@ -3,11 +3,18 @@
 In addition to the date, I added table filters for the city, state, country, and shape. :compass: 
 
 ## Resources
+Javascript, CSS, HTML
 
 ## Results/Analysis
-Describe to Dana how someone might use the new webpage by walking her through the process of using the search criteria. Use images of your webpage during the filtering process to support your explanation.
-There is a description of how to perform a search, with images.
+Welcome to the UFO Sightings page!
+In order to begin your search: please float on over to the filtered search section and in lowercase letters enter in the date, city, state, country or shape and click filter.
+The website will generate results based on the information you provide. To reset the filter criteria, click the UFO Sightings at the top left of the website.
 
-## Summary
-The summary addresses one drawback of this webpage (2 pt)
-The summary addresses two additional recommendations for further development
+## Summary/Areas for Improvement:
+The user must know specific dates, cities, or shapes to search. Some shapes like "light" might not be as intuitive. The filters require correct lower-case spellings and cannot include spaces at the end. 
+The city that was used, for example, could not be typed as "elcajon", “el cajon_”, or "El Cajon". The only acceptable input would be "el cajon".
+
+Two recommendations are:
+- Make the page upper case/lower case intuitive. If a search were to be for Roswell rather than roswell it would be syntactically correct.
+- Allow for a map point to appear when the search results are populated. A google map location would be beneficialto anyone interested in the general global/topography of a search result. 
+- A fun bonus to the page would be as the mouse navigates the page add a whimsical ufo icon that travels as the mouse tail.
---
+++ static/js/appmodwork.js
@@ -0,0 +1,137 @@
+// import the data from data.js
+const tableData = data;
+
+// Reference the HTML table using d3
+var tbody = d3.select("tbody");
+
+// Simple JavaScript console.log statement
+function printHello();
+
+// Simple JavaScript console.log statement
+function printHello() {
+console.log("Hello there!");
+  };
+
+// Takes two numbers and adds them
+function addition(a, b) {
+    return a + b;
+  };
+console.log(addition(a, b));
+
+  // Functions can call other functions
+function doubleAddition(c, d) {
+    var total = addition(c, d) * 2;
+    return total;
+  };
+    // Functions can call other functions
+function doubleAddition(4, 5) {
+  var total = addition(4, 5) * 2;
+  return total;
+};
+
+  // Simple JavaScript log statement
+function printHello() {
+    return "Hello there!";
+  };
+  printHello = () => "Hello there!";
+
+  // Original addition function
+function addition(a, b) {
+    return a + b;
+  };
+ // Converted to an arrow function
+addition = (a, b) => a + b; 
+
+// Original doubleAddition function
+function doubleAddition(c, d) {
+    var total = addition(c, d) * 2;
+    return total;
+  };
+
+  // converted to arrow
+  doubleAddition = (c, d)=> addition(c, d) * 2;
+
+  //skill drill Use the newly refactored doubleAddition function to find the total of 33 and 25.
+  doubleAddition = (35, 25)=> addition(35, 25) * 2;
+
+  let friends = ["Sarah", "Greg", "Cindy", "Jeff"];
+  function listLoop(userList) {
+    for (var i = 0; i < userList.length; i++) {
+      console.log(userList[i]);
+    }
+};
+
+ // to clear browser
+clear()
+
+//forloops
+let friends = ["Sarah", "Greg", "Cindy", "Jeff"];
+function listLoop(userList) {
+  for (var i = 0; i < userList.length; i++) {
+    console.log(userList[i]);
+  }
+};
+
+var vegetables = ["Carrots", "Peas", "Lettuce", "Tomatoes"];
+
+for (var i = 0; i < vegetables.length; i++) {
+    console.log("I love " + vegetables[i]);
+};
+
+for (var i = 0; i < 5; i++) {
+  console.log("I am " + i);
+};
+
+//BUILD TABLE step 1
+function buildTable(data) {
+  // First, clear out any existing data
+  tbody.html("");
+
+  // Next, loop through each object in the data
+  // and append a row and cells for each value in the row
+  data.forEach((dataRow) => {
+    // Append a row to the table body
+    let row = tbody.append("tr");
+
+    // Loop through each field in the dataRow and add
+    // each value as a table cell (td)
+    Object.values(dataRow).forEach((val) => {
+      let cell = row.append("td");
+      cell.text(val);
+      }
+    );
+  });
+};
+
+//handleClick (create button)
+function handleClick() {
+  // Grab the datetime value from the filter
+  let date = d3.select("#datetime").property("value");
+  let filteredData = tableData;
+
+   // Check to see if a date was entered and filter the
+  // data using that date.
+  if (date) {
+    // Apply `filter` to the table data to only keep the
+    // rows where the `datetime` value matches the filter value
+    filteredData = filteredData.filter(row => row.datetime === date);
+  }
+
+   // Rebuild the table using the filtered data
+  // @NOTE: If no date was entered, then filteredData will
+  // just be the original tableData.
+  buildTable(filteredData);
+}
+  // Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+  
+  // Build the table when the page loads
+  buildTable(tableData);
+
+
+
+// If statement   // if-statement syntax
+//if ( condition ) { code to execute }  
+    // pseudocode practice
+//if (a date is entered) {
+  //Filter the default data to show only the date entered};
